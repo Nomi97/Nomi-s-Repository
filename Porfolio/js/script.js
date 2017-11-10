90528c9198b18416da7ba3f1d74794717ca2158c
Nomi <Noman1997@hotmail.se>
2017-11-10 17:36:27 +0100

    Portfolio

---
+++ Porfolio/js/script.js
@@ -0,0 +1,32 @@
+﻿$(function() {
+    $(document).scroll(function() {
+        var nav = $("nav");
+        var home = $('#home');
+        nav.toggleClass('scrolled', $(this).scrollTop() > home.height());
+    }); // menu scroll effect
+
+
+
+
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 400, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    }); // navigation scroll effect
+});
